f16e7994798c1bc258adba99d8cd5ae73eb84634
Haris Afridi <haris.afridi1312@gmail.com>
2026-06-19 16:02:07 +0500

    deploy dist

--- src/components/Blobs.jsx
+++
@@ -1,9 +0,0 @@
-export default function Blobs() {
-  return (
-    <>
-      <div className="blob blob-1" />
-      <div className="blob blob-2" />
-      <div className="blob blob-3" />
-    </>
-  );
-}
